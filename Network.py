97e5667272ed7c6cab4c549dd6eb74c754ab2f4f
Asher <smasherisaac@gmail.com>
2025-07-21 21:16:25 +1000

    Add documentation to Network

--- Network.py
+++ Network.py
@@ -17,29 +17,58 @@ class Network:
         self.layers: List[Layer] = []
         self._is_compiled = False
 
-    def add(self, *layers: Layer):
+    def add(self, *layers: Layer) -> None:
+        """
+        Adds layers to the network.
+
+        :param layers: One or more Layer instances to be added to the network.
+        """
         for layer in layers: self.layers.append(layer)
 
     def __add__(self, other: Layer):
         self.layers.append(other)
 
-    def save_weights(self, path):
+    def save_weights(self, path) -> None:
+        """
+        Saves the weights of all layers in the network to different files in the specified path.
+
+        :param path: The directory or where the weights will be saved.
+        """
         for i, layer in enumerate(self.layers):
             layer.save_weights(path, f"Layer{i}")
 
     def network_forward(self, inputs: np.ndarray) -> np.ndarray:
+        """
+        Performs a forward pass through the neural network.
+        :param inputs: Input data to be passed through the network.
+        :return: The output of the network after processing the input through all layers.
+        """
         layer_output = inputs
         for layer in self.layers:
             layer_output = layer.feedforward(layer_output)
 
         return layer_output
 
-    def network_backward(self, d_output: np.ndarray):        
+    def network_backward(self, d_output: np.ndarray) -> None:
+        """
+        Performs the backward pass through the network, updating internal weights.
+
+        :param d_output: The gradient of the loss with respect to the network's output.
+        """
         for layer in self.layers[::-1]:
             d_output = layer.backward_propagation(d_output)
             layer.update_parameters()
 
     def train(self, inputs: np.ndarray, expected_output: np.ndarray, iterations: int = 10000) -> List[float]:
+        """
+        Trains the neural network using the provided input data and expected output.
+
+        :param inputs: Input data for training, typically of shape (num_samples, num_features).
+        :param expected_output: Expected output (labels or targets) corresponding to the input data.
+        :param iterations: Number of training iterations. Defaults to 10000.
+
+        :return: List of cost values recorded at regular intervals during training.
+        """
         costs = []
 
         for i in range(iterations):
@@ -56,6 +85,14 @@ class Network:
         return costs
 
     def predict(self, inputs: np.ndarray):
+        """
+        Predicts the output class for the given input data using the trained network.
+
+        :param inputs: Input data to be evaluated by the network.
+
+        :return: Boolean array indicating whether each network output exceeds the threshold.
+        """
+
         network_output = self.network_forward(inputs)
         return network_output > self.threshold
 
